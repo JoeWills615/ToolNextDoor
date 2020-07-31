2410f7c49ebd469220bb3dc7849b0c569fd34c71
Chris Ferguson <cfergus7@gmail.com>
2020-07-31 15:00:43 -0500

    about page

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -17,6 +17,9 @@ function NavBar() {
                     <li className="nav-item">
                         <a className="nav-link" href="/">Login</a>
                     </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/about">About</a>
+                    </li>
                 </ul>
             </div>
         </nav>)
