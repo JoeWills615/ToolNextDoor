1fe50867b31c3b16a2e98a3ff84d17bdb3d45912
Joe Wills <JGW3Code@gmail.com>
2020-08-03 10:41:15 -0500

    TOOLution added

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -19,7 +19,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Tool Next Door
+      Chris/Alex/Lex/Joe
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
